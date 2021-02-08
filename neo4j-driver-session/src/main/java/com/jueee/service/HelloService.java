5e141dad6aba29b6371af2f2746187c1743917e3
尉勇强2 <hzweiyongqiang@corp.netease.com>
2021-02-08 16:34:01 +0800

    update_2021-02-08

--- neo4j-driver-session/src/main/java/com/jueee/service/HelloService.java
+++ neo4j-driver-session/src/main/java/com/jueee/service/HelloService.java
@@ -7,7 +7,7 @@ import org.neo4j.driver.*;
 public class HelloService {
 
     public static void main(String[] args) {
-        Driver driver = GraphDatabase.driver("bolt://10.196.8.149:7687", AuthTokens.basic("neo4j", "123456"));
+        Driver driver = GraphDatabase.driver("bolt://neo4j.host.address:7687", AuthTokens.basic("neo4j", "123456"));
         Session session = driver.session();
         final Result result = session.run("MATCH (a:Person) RETURN a.name AS name");
         while (result.hasNext()) {
